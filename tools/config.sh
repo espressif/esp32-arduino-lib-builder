c0043da5cb49fbb340a368801609022a8911d71c
Jake <git@ja-ke.tech>
2021-01-17 02:20:46 +0100

    Replace `arch` with `uname -m`
    https://www.gnu.org/software/coreutils/manual/html_node/arch-invocation.html

--- tools/config.sh
+++ tools/config.sh
@@ -38,7 +38,7 @@ AR_GEN_PART_PY="$AR_TOOLS/gen_esp32part.py"
 AR_SDK="$AR_TOOLS/sdk"
 
 function get_os(){
-  	OSBITS=`arch`
+  	OSBITS=`uname -m`
   	if [[ "$OSTYPE" == "linux"* ]]; then
         if [[ "$OSBITS" == "i686" ]]; then
         	echo "linux32"
