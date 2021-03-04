1bf7b2659710b72aa0def392579c02917a43a8bb
me-no-dev <hristo@espressif.com>
2021-03-04 14:53:25 +0200

    Update prepare-ci.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python python-setuptools python-cryptography python-pyparsing python-pyelftools cmake ninja-build ccache
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py && \
-pip3 install pyserial click future wheel
+sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
+pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
