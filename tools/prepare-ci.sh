cc1deacd9a0f56e058d2d7a28679bdfa9df175d7
me-no-dev <hristo@espressif.com>
2021-04-05 14:29:50 +0300

    Fix CI

---
+++ tools/prepare-ci.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
+pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
