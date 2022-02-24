cd5d3b2e7dceac38ea2c81496ef1e2ebb7412857
me-no-dev <hristo@espressif.com>
2022-02-24 02:30:58 +0200

    Allow selecting Arduino branch based on the current lib-builder branch

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -18,19 +18,26 @@ if [ ! -d "$AR_COMPS/arduino" ]; then
 fi
 
 if [ -z $AR_BRANCH ]; then
-	has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "idf-$IDF_BRANCH"`
-	if [ "$has_ar_branch" == "1" ]; then
-		export AR_BRANCH="idf-$IDF_BRANCH"
+	current_branch=`git branch --show-current`
+	if [[ "$current_branch" != "master" && `git_branch_exists "$AR_COMPS/arduino" "$current_branch"` == "1" ]]; then
+		export AR_BRANCH="$current_branch"
 	else
-		has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "$AR_PR_TARGET_BRANCH"`
+		has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "idf-$IDF_BRANCH"`
 		if [ "$has_ar_branch" == "1" ]; then
-			export AR_BRANCH="$AR_PR_TARGET_BRANCH"
+			export AR_BRANCH="idf-$IDF_BRANCH"
+		else
+			has_ar_branch=`git_branch_exists "$AR_COMPS/arduino" "$AR_PR_TARGET_BRANCH"`
+			if [ "$has_ar_branch" == "1" ]; then
+				export AR_BRANCH="$AR_PR_TARGET_BRANCH"
+			fi
 		fi
 	fi
 fi
 
 if [ "$AR_BRANCH" ]; then
-	git -C "$AR_COMPS/arduino" checkout "$AR_BRANCH"
+	git -C "$AR_COMPS/arduino" checkout "$AR_BRANCH" && \
+	git -C "$AR_COMPS/arduino" fetch && \
+	git -C "$AR_COMPS/arduino" pull --ff-only
 fi
 if [ $? -ne 0 ]; then exit 1; fi
 
