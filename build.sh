cb2abf577a159dc839dd3e6690929eac3a7d3069
me-no-dev <hristo@espressif.com>
2022-03-18 17:44:22 +0200

    Slight adjustment in the build scripts

--- build.sh
+++ build.sh
@@ -84,7 +84,7 @@ if [ $SKIP_ENV -eq 0 ]; then
     ./tools/update-components.sh
     if [ $? -ne 0 ]; then exit 1; fi
 
-    # install esp-idf and gcc toolchain
+    # install esp-idf
     source ./tools/install-esp-idf.sh
     if [ $? -ne 0 ]; then exit 1; fi
 else
