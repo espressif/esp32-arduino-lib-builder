8fce687bf7a8c69aea6129a15feaca379f916f06
me-no-dev <hristo@espressif.com>
2023-04-12 01:28:26 +0300

    Push to esp32-arduino-libs

--- tools/push-to-arduino.sh
+++ tools/push-to-arduino.sh
@@ -51,4 +51,69 @@ if [ "$AR_HAS_PR" == "0" ]; then
 		exit 1
 	fi
 fi
+
+#
+# esp32-arduino-libs
+#
+cd "$AR_ROOT"
+IDF_LIBS_DIR="$AR_ROOT/../esp32-arduino-libs"
+
+if [ ! -d "$IDF_LIBS_DIR" ]; then
+	echo "Cloning esp32-arduino-libs..."
+	git clone "https://github.com/espressif/esp32-arduino-libs.git" "$IDF_LIBS_DIR"
+else
+	echo "Updating esp32-arduino-libs..."
+	git -C "$IDF_LIBS_DIR" fetch && \
+	git -C "$IDF_LIBS_DIR" pull --ff-only
+fi
+if [ $? -ne 0 ]; then exit 1; fi
+
+LIBS_HAS_COMMIT=`git_commit_exists "$IDF_LIBS_DIR" "$AR_NEW_COMMIT_MESSAGE"`
+LIBS_HAS_BRANCH=`git_branch_exists "$IDF_LIBS_DIR" "$AR_NEW_BRANCH_NAME"`
+
+if [ "$LIBS_HAS_COMMIT" == "1" ]; then
+	echo "Commit '$AR_NEW_COMMIT_MESSAGE' in esp32-arduino-libs Already Exists"
+	mkdir -p dist && echo "Commit '$AR_NEW_COMMIT_MESSAGE' Already Exists" > dist/log.txt
+	exit 0
+fi
+
+if [ "$LIBS_HAS_BRANCH" == "1" ]; then
+	echo "Branch '$AR_NEW_BRANCH_NAME' Already Exists"
+	echo "Switching to esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+	git -C "$IDF_LIBS_DIR" checkout $AR_NEW_BRANCH_NAME
+else
+	echo "Creating esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+	git -C "$IDF_LIBS_DIR" checkout -b $AR_NEW_BRANCH_NAME
+fi
+if [ $? -ne 0 ]; then
+    echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
+	exit 1
+fi
+
+if [ $LIBS_HAS_COMMIT == "0" ]; then
+	# make changes to the files
+	echo "Patching files in esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+	rm -rf $IDF_LIBS_DIR/sdk && cp -Rf $AR_TOOLS/sdk $IDF_LIBS_DIR/
+	
+	cd $IDF_LIBS_DIR
+
+	# did any of the files change?
+	if [ -n "$(git status --porcelain)" ]; then
+		echo "Pushing changes to esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'..."
+	    git add . && git commit --message "$AR_NEW_COMMIT_MESSAGE" && git push -u origin $AR_NEW_BRANCH_NAME
+		if [ $? -ne 0 ]; then
+		    echo "ERROR: Pushing to branch '$AR_NEW_BRANCH_NAME' failed"
+			exit 1
+		fi
+	else
+	    echo "No changes in esp32-arduino-libs branch '$AR_NEW_BRANCH_NAME'"
+	    if [ $LIBS_HAS_BRANCH == "0" ]; then
+	    	echo "Delete created branch '$AR_NEW_BRANCH_NAME'"
+	    	git branch -d $AR_NEW_BRANCH_NAME
+	    fi
+	    exit 0
+	fi
+fi
+
+
 exit 0
