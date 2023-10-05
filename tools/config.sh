7b3f7e5c5e8f6d46965714d66831f6315ad41be1
me-no-dev <hristo@espressif.com>
2023-10-05 14:56:25 +0300

    Update target branch

--- tools/config.sh
+++ tools/config.sh
@@ -10,8 +10,7 @@ if [ -z $IDF_BRANCH ]; then
 fi
 
 if [ -z $AR_PR_TARGET_BRANCH ]; then
-    # Temporary to get CI working. original is master
-    AR_PR_TARGET_BRANCH="esp-idf-v5.1-libs"
+    AR_PR_TARGET_BRANCH="master"
 fi
 
 if [ -z $IDF_TARGET ]; then
