cd6f3bddf77efcbcbd0ad7446db86c995fff4df2
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-02-08 17:09:28 -0300

    Test docker Image

---
+++ tools/docker/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# LIBBUILDER_GIT_SAFE_DIR has the same format as system PATH environment variable.
+# All path specified in LIBBUILDER_GIT_SAFE_DIR will be added to user's
+# global git config as safe.directory paths. For more information
+# see git-config manual page.
+if [ -n "${LIBBUILDER_GIT_SAFE_DIR+x}" ]
+then
+	echo "Adding following directories into git's safe.directory"
+	echo "$LIBBUILDER_GIT_SAFE_DIR" | tr ':' '\n' | while read -r dir
+	do
+		git config --global --add safe.directory "$dir"
+		echo "  $dir"
+	done
+fi
+
+exec "$@"
