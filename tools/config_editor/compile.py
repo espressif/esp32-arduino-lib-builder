8caa77e033a75d02f5d8f05baf73c82935a433b8
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-03-12 14:00:37 -0300

    Improve style

--- tools/config_editor/compile.py
+++ tools/config_editor/compile.py
@@ -17,31 +17,55 @@ class CompileScreen(Screen):
     # Child process running the libraries compilation
     child_process = None
 
-    def print_output(self, renderable: RenderableType) -> None:
+    log_widget: RichLog
+    button_widget: Button
+
+    def print_output(self, renderable: RenderableType, style=None) -> None:
         # Print output to the RichLog widget
-        self.query_one(RichLog).write(renderable)
+        if style is None:
+            self.log_widget.write(renderable)
+        else:
+            # Check the available styles at https://rich.readthedocs.io/en/stable/style.html
+            self.log_widget.write("[" + str(style) + "]" + renderable)
+
+    def print_error(self, error: str) -> None:
+        # Print error to the RichLog widget
+        self.log_widget.write("[b bright_red]" + error)
+        self.button_widget.add_class("-error")
+        #print("Error: " + error) # For debugging
+
+    def print_success(self, message: str) -> None:
+        # Print success message to the RichLog widget
+        self.log_widget.write("[b bright_green]" + message)
+        self.button_widget.add_class("-success")
+        #print("Success: " + message) # For debugging
+
+    def print_info(self, message: str) -> None:
+        # Print info message to the RichLog widget
+        self.log_widget.write("[b bright_cyan]" + message)
+        #print("Info: " + message) # For debugging
 
     @work(name="compliation_worker", group="compilation", exclusive=True, thread=True)
     def compile_libs(self) -> None:
         # Compile the libraries
+        print("Starting compilation process")
 
         label = self.query_one("#compile-title", Static)
         self.child_process = None
         target = self.app.setting_target
 
-        print("Compiling for " + target.upper())
         label.update("Compiling for " + target.upper())
-        self.print_output("======== Compiling for " + target.upper() + " ========")
+        self.print_info("======== Compiling for " + target.upper() + " ========")
 
         command = ["./build.sh", "-t", target, "-D", self.app.setting_debug_level]
+
         #command.append("--help") # For testing output without compiling
 
         if self.app.setting_enable_copy:
             if os.path.isdir(self.app.setting_arduino_path):
                 command.extend(["-c", self.app.setting_arduino_path])
             else:
-                print("Invalid path to Arduino core: " + self.app.setting_arduino_path)
-                self.print_output("Invalid path to Arduino core: " + self.app.setting_arduino_path)
+                self.print_error("Invalid path to Arduino core: " + self.app.setting_arduino_path)
                 label.update("Invalid path to Arduino core")
                 return
 
@@ -54,8 +78,7 @@ class CompileScreen(Screen):
         if self.app.setting_idf_commit:
             command.extend(["-i", self.app.setting_idf_commit])
 
-        self.print_output("Running: " + " ".join(command) + "\n")
-        print("Running: " + " ".join(command))
+        self.print_info("Running: " + " ".join(command) + "\n")
         self.child_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         try:
             for output in self.child_process.stdout:
@@ -69,19 +92,25 @@ class CompileScreen(Screen):
             print("Process might have terminated")
 
         if not self.child_process:
-            print("Compilation failed for " + target.upper() + "Child process failed to start")
+            self.print_error("Compilation failed for " + target.upper() + "Child process failed to start")
             label.update("Compilation failed for " + target.upper() + "Child process failed to start")
             return
         else:
             self.child_process.wait()
 
         if self.child_process.returncode != 0:
-            print("Compilation failed for " + target.upper() + ". Return code: " + str(self.child_process.returncode))
-            self.print_output("Compilation failed for " + target.upper() + ". Return code: " + str(self.child_process.returncode))
-            self.print_output("Error: " + self.child_process.stderr.read())
+            self.print_error("Compilation failed for " + target.upper() + ". Return code: " + str(self.child_process.returncode))
+            self.print_error("Errors:")
+            try:
+                for error in self.child_process.stderr:
+                    if error:
+                        self.print_error(error.strip())
+                self.child_process.stderr.close()
+            except Exception as e:
+                print("Error reading child process errors: " + str(e))
             label.update("Compilation failed for " + target.upper())
         else:
-            print("Compilation successful for " + target.upper())
+            self.print_success("Compilation successful for " + target.upper())
             label.update("Compilation successful for " + target.upper())
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
@@ -103,16 +132,19 @@ class CompileScreen(Screen):
     def on_resume(self) -> None:
         # Event handler called every time the screen is activated
         print("Compile screen resumed. Clearing logs and starting compilation process")
-        log = self.query_one(RichLog)
-        log.clear()
-        log.focus()
+        self.button_widget.remove_class("-error")
+        self.button_widget.remove_class("-success")
+        self.log_widget.clear()
+        self.log_widget.focus()
         self.compile_libs()
 
     def compose(self) -> ComposeResult:
         # Compose the compilation screen
         yield Header()
         with Container(id="compile-log-container"):
-            yield RichLog(markup=True, id="compile-log")
+            self.log_widget = RichLog(markup=True, id="compile-log")
+            yield self.log_widget
         with Container(id="compile-status-container"):
             yield Static("Compiling for ...", id="compile-title")
-            yield Button("Back", id="compile-back-button")
+            self.button_widget = Button("Back", id="compile-back-button")
+            yield self.button_widget
