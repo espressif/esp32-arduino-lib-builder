a11164ca7258eeeb7a8d7298a558fccc79be7c6a
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-04-09 10:02:00 -0300

    Add checks to entrypoint

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -7,19 +7,19 @@ set -e
 # see git-config manual page.
 if [ -n "${LIBBUILDER_GIT_SAFE_DIR+x}" ]
 then
-	echo "Adding following directories into git's safe.directory"
-	echo "$LIBBUILDER_GIT_SAFE_DIR" | tr ':' '\n' | while read -r dir
-	do
-		git config --global --add safe.directory "$dir"
-		echo "  $dir"
-	done
+    echo "Adding following directories into git's safe.directory"
+    echo "$LIBBUILDER_GIT_SAFE_DIR" | tr ':' '\n' | while read -r dir
+    do
+        git config --global --add safe.directory "$dir"
+        echo "  $dir"
+    done
 fi
 
 # Check if the mount point /arduino-esp32 exists
-if [ -d "/arduino-esp32" ]; then
-    # If it exists, add the -p and -c arguments
+if [ -d "/arduino-esp32" ] && [[ "$@" == "python3 tools/config_editor/app.py"* ]]; then
+    # Running UI with mount point detected, adding -p and -c arguments
     exec "$@" -c /arduino-esp32 -p `stat -c "%u:%g" /arduino-esp32`
 else
-    # If it doesn't exist, just execute the command without them
+    # Running UI without mount point detected or running another command
     exec "$@"
 fi
