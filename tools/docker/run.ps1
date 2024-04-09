c4f4ce3d4b8b89f65d8740f5860deac475ea9f2c
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-04-09 10:47:30 -0300

    Test example run scripts

--- tools/docker/run.ps1
+++ tools/docker/run.ps1
@@ -1,18 +1,31 @@
-$ARDUINO_DIR = $args[0] -replace '\\', '/'
-if (-not $ARDUINO_DIR) {
-    $ARDUINO_DIR = (Get-Location).Path + '/../'
+# This is an example of how to run the docker container.
+# This script is not part of the container, it is meant to be run on the host machine.
+# Usage: ./run.ps1 <path_to_arduino_esp32>
+
+if (-not (Test-Path -Path (Get-Command docker).Source)) {
+    Write-Host "ERROR: Docker is not installed! Please install docker first."
+    exit 1
 }
 
-$ARDUINO_DIR = (Get-Item -Path $ARDUINO_DIR).FullName
+if ($args.Count -gt 0) {
+    $ARDUINO_DIR = $args[0] -replace '\\', '/'
+} else {
+    $ARDUINO_DIR = ''
+}
+
 $DOCKER_ARGS = @()
 $DOCKER_ARGS += '-it'
-$DOCKER_ARGS += '-v', "$ARDUINO_DIR:/arduino-esp32"
 $DOCKER_ARGS += '-e', 'TERM=xterm-256color'
-$DOCKER_ARGS += '-e', "HOST_UID=$env:UID"
+
+if ((Test-Path $ARDUINO_DIR)) {
+    $DOCKER_ARGS += '-v', "$ARDUINO_DIR:/arduino-esp32"
+} else {
+    Write-Output "Warning: Invalid arduino directory: '$ARDUINO_DIR'. Ignoring it."
+}
 
 if ($env:LIBBUILDER_GIT_SAFE_DIR) {
     $DOCKER_ARGS += '-e', "LIBBUILDER_GIT_SAFE_DIR=$env:LIBBUILDER_GIT_SAFE_DIR"
 }
 
-Write-Output "Running: docker run $($DOCKER_ARGS -join ' ') lucassvaz/esp32-arduino-lib-builder:latest"
-docker run @($DOCKER_ARGS) lucassvaz/esp32-arduino-lib-builder:latest python3 tools/config_editor/app.py
+Write-Output "Running: docker run $($DOCKER_ARGS -join ' ') lucassvaz/esp32-arduino-lib-builder"
+#docker run @($DOCKER_ARGS) lucassvaz/esp32-arduino-lib-builder
