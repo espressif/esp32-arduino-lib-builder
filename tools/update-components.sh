5197432d08e23ffaf6402acb97e3bfaf80a1b330
twischi <thomas@wisniewski.info>
2024-05-25 22:37:32 +0200

    finalized Color

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -9,7 +9,7 @@ echo "...Component TinyUSB installing/updating local copy...."
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 
-echo -e "   cloning $eGI $TINYUSB_REPO_URL $eNO\n   to:$ePF $TINYUSB_REPO_DIR $eNO"
+echo -e "   cloning$eGI$TINYUSB_REPO_URL $eNO\n   to:$ePF$TINYUSB_REPO_DIR $eNO"
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"  --quiet
 else
