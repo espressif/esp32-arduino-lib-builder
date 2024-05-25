4848b91cc99166c065bec610fcf75957188f0501
twischi <thomas@wisniewski.info>
2024-05-25 20:18:40 +0200

    added option

--- myBuild.sh
+++ myBuild.sh
@@ -3,4 +3,4 @@
 rm -rf /Users/thomas/esp/arduino-esp32
 mkdir /Users/thomas/esp/arduino-esp32
 
-./build.sh -S -t 'esp32h2,esp32s2,esp32c2,esp32' -A 'idf-release/v5.1' -I 'release/v5.1' -e -D 'error' -c '/Users/thomas/esp/arduino-esp32'
+./build.sh -t 'esp32h2,esp32s2,esp32c2,esp32' -A 'idf-release/v5.1' -I 'release/v5.1' -e -D 'error' -c '/Users/thomas/esp/arduino-esp32' -S -W
