b81e5187498ba73dadf32fb65509362af72ccd0c
twischi <thomas@wisniewski.info>
2024-05-26 13:54:03 +0200

    error fix

--- yourBuild_with_log.sh
+++ yourBuild_with_log.sh
@@ -3,7 +3,7 @@
 # ---------------------------------------
 # *** Set the Folder for the Log File *** 
 # ---------------------------------------
-logFolder=logFolder=$(pwd)/libBuildLogs && logFolder=$(eval echo "$logFolder") # Set the Folder for the Log File
+logFolder=$(pwd)/libBuildLogs && logFolder=$(eval echo "$logFolder") # Set the Folder for the Log File
 
 #---------------------------------------------------------------------------------- 
 # Create a new file with the current timestamp to take the output of an Bash sricpt
@@ -23,3 +23,8 @@ mkdir /Users/thomas/esp/arduino-esp32
 # RUN your build script with LogFile '2>&1 | tee $logFile'  # Echo a text to the LogFile and Terminal 
 ./build.sh -t 'esp32h2' -A 'idf-release/v5.1' -I 'release/v5.1' -e -D 'error' -c '/Users/thomas/esp/arduino-esp32' -S -V  2>&1 | tee $logFile
 #./build.sh -t 'esp32h2,esp32s2,esp32c2,esp32' -A 'idf-release/v5.1' -I 'release/v5.1' -e -D 'error' -c '/Users/thomas/esp/arduino-esp32' -S -V  2>&1 | tee $logFile
+
+# ---------------------------------------
+#           REPLAY a LOG FILE
+# ---------------------------------------
+#echo "$(<$logFile)" 
