19d9bdc38caf2b7c6406a574338caa0ab633e092
twischi <thomas@wisniewski.info>
2024-05-28 01:30:14 +0200

    test with none

--- yourBuild_with_log.sh
+++ yourBuild_with_log.sh
@@ -35,10 +35,11 @@ echo -e "-- Logging to\n   Folder:$ePF $logFolder $eNO"
 #./build.sh -t 'esp32h2,esp32s2,esp32c2,esp32' -A 'idf-release/v5.1' -I 'release/v5.1' -e -D 'error' -c '/Users/thomas/esp/arduino-esp32' -S -V  2>&1 | tee $logFile
 
 # Build for all ESP32 variants with full output
-./build.sh -t "esp32h2" -A "idf-release/v5.1" -I "release/v5.1" -e -D "error" -c "/Users/thomas/esp/to_arduino-esp32_$timestamp" -W 2>&1 | tee $logFile
+./build.sh -t "esp32h2" -A "idf-release/v5.1" -I "release/v5.1" -e -D "none" -c "/Users/thomas/esp/to_arduino-esp32_$timestamp"  2>&1 | tee $logFile
 
 #-t 'esp32h2
 #-I 'release/v5.1'
+# -D "error"
 #-i "d7b0a45"
 
 
