8d53d81854b3d3536d8a24a34146babbaec1375f
twischi <thomas@wisniewski.info>
2024-06-08 16:10:21 +0200

    usage of shortFP

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -12,10 +12,10 @@ TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 
 # Check if the directory exists and clone or update
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-    echo -e "   cloning$eGI $TINYUSB_REPO_URL $eNO\n   to:$ePF $TINYUSB_REPO_DIR $eNO"    
+    echo -e "   cloning$eGI $TINYUSB_REPO_URL $eNO\n   to: $(shortFP $TINYUSB_REPO_DIR)"
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"  --quiet
 else
-    echo -e "   updating (already there)$eGI $TINYUSB_REPO_URL $eNO\n   to:$ePF $TINYUSB_REPO_DIR $eNO"
+    echo -e "   updating (already there)$eGI $TINYUSB_REPO_URL $eNO\n   to: $(shortFP $TINYUSB_REPO_DIR)"
     git -C "$TINYUSB_REPO_DIR" fetch --quiet && \
     git -C "$TINYUSB_REPO_DIR" pull --ff-only --quiet
 fi
