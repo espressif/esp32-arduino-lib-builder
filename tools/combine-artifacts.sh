708b55d522b229d167bcb2eb7c2eb16289208c8c
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-07-19 11:07:00 -0300

    Fix push

--- tools/combine-artifacts.sh
+++ tools/combine-artifacts.sh
@@ -8,7 +8,7 @@ libs_folder="out/tools/esp32-arduino-libs"
 files=$(find dist -name 'arduino-esp32-libs-esp*.tar.gz')
 for file in $files; do
     echo "Extracting $file"
-    tar zxvf $file -C out
+    tar zxf $file -C out
     cat $libs_folder/versions.txt >> $libs_folder/versions_full.txt
 done
 
@@ -16,4 +16,5 @@ done
 awk -i inplace '!seen[$0]++' $libs_folder/versions_full.txt
 mv -f $libs_folder/versions_full.txt $libs_folder/versions.txt
 
-cd out/tools && zip -r ../../dist/esp32-arduino-libs.zip * && cd ../..
+echo "Creating zip file"
+cd out/tools && zip -q -r ../../dist/esp32-arduino-libs.zip * && cd ../..
--- tools/push-to-arduino.sh
+++ tools/push-to-arduino.sh
@@ -25,107 +25,91 @@ LIBS_JSON_FILENAME="package-$LIBS_VERSION.json"
 IDF_LIBS_ZIP_URL="https://github.com/$AR_LIBS_REPO/releases/download/$LIBS_RELEASE_TAG/$LIBS_ZIP_FILENAME"
 IDF_LIBS_JSON_URL="https://github.com/$AR_LIBS_REPO/releases/download/$LIBS_RELEASE_TAG/$LIBS_JSON_FILENAME"
 
-if [ $AR_HAS_COMMIT == "0" ]; then
-	if [ $LIBS_HAS_ASSET == "0" ]; then
-		cd "$AR_ROOT"
-		mkdir -p dist
+if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
+	cd "$AR_ROOT"
+	mkdir -p dist
 
-		# check if the release exists
-		if [ $LIBS_HAS_RELEASE == "0" ]; then
-			echo "Release for tag \"$LIBS_RELEASE_TAG\" not found. Please create the release first."
-			exit 1
+	# check if the release exists
+	if [ $LIBS_HAS_RELEASE == "0" ]; then
+		echo "Release for tag \"$LIBS_RELEASE_TAG\" not found. Please create the release first."
+		exit 1
+	fi
+
+	# Delete old assets for the version
+	if [ $LIBS_HAS_ASSET == "1" ]; then
+		echo "Deleting existing assets for version '$LIBS_VERSION'..."
+		if [ `github_release_asset_delete "$AR_LIBS_REPO" "$LIBS_ASSET_ID"` == "0" ]; then
+			echo "ERROR: Failed to delete asset '$LIBS_ZIP_FILENAME'"
 		fi
-
-		echo "Creating asset '$LIBS_ZIP_FILENAME'..."
-
-		mv -f "dist/esp32-arduino-libs.zip" "dist/$LIBS_ZIP_FILENAME"
-		LIBS_ASSET_ID=`github_release_asset_upload "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_ZIP_FILENAME" "dist/$LIBS_ZIP_FILENAME"`
-		if [ -z "$LIBS_ASSET_ID" ]; then
-			echo "ERROR: Failed to upload asset '$LIBS_ZIP_FILENAME'"
-			exit 1
-		fi
-
-		echo "Finished uploading asset '$LIBS_ZIP_FILENAME'. Asset ID: $LIBS_ASSET_ID"
-
-		# Calculate the local file checksum and size
-		local_checksum=$(sha256sum "dist/$LIBS_ZIP_FILENAME" | awk '{print $1}')
-		local_size=$(stat -c%s "dist/$LIBS_ZIP_FILENAME")
-
-		echo "Downloading asset '$LIBS_ZIP_FILENAME' and checking integrity..."
-
-		# Download the file
-		remote_file="remote-$LIBS_ZIP_FILENAME"
-		curl -s -L -o "$remote_file" "$IDF_LIBS_ZIP_URL"
-
-		# Check if the download was successful
-		if [ $? -ne 0 ]; then
-			echo "Error downloading file from $IDF_LIBS_ZIP_URL"
-			exit 1
-		fi
-
-		# Calculate the remote file checksum and size
-		remote_checksum=$(sha256sum "$remote_file" | awk '{print $1}')
-		remote_size=$(stat -c%s "$remote_file")
-
-		echo "Local: $local_size bytes, $local_checksum"
-		echo "Remote: $remote_size bytes, $remote_checksum"
-
-		# Check if the checksums match
-		if [ "$local_checksum" != "$remote_checksum" ]; then
-			echo "Checksum mismatch for downloaded file"
-			echo "Deleting asset and exiting..."
-			if [ `github_release_asset_delete "$AR_LIBS_REPO" "$LIBS_ASSET_ID"` == "0" ]; then
-				echo "ERROR: Failed to delete asset '$LIBS_ZIP_FILENAME'"
-			fi
-			exit 1
-		fi
-
-		# Clean up the downloaded file
-		rm "$remote_file"
-
-		# Print the results
-		echo "Tool: esp32-arduino-libs"
-		echo "Version: $LIBS_VERSION"
-		echo "URL: $IDF_LIBS_ZIP_URL"
-		echo "File: $LIBS_ZIP_FILENAME"
-		echo "Size: $local_size bytes"
-		echo "SHA-256: $local_checksum"
-		echo "JSON: $AR_OUT/package_esp32_index.template.json"
-		cd "$AR_ROOT"
-		python3 tools/add_sdk_json.py -j "$AR_OUT/package_esp32_index.template.json" -n "esp32-arduino-libs" -v "$LIBS_VERSION" -u "$IDF_LIBS_ZIP_URL" -f "$LIBS_ZIP_FILENAME" -s "$local_size" -c "$local_checksum"
-		if [ $? -ne 0 ]; then exit 1; fi
-
-		JSON_ASSET_ID=`github_release_asset_upload "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_JSON_FILENAME" "$AR_OUT/package_esp32_index.template.json"`
-		if [ -z "$JSON_ASSET_ID" ]; then
-			echo "ERROR: Failed to upload asset '$LIBS_JSON_FILENAME'"
-			exit 1
-		fi
-	else
-		cd "$AR_ROOT"
-		mkdir -p $AR_TOOLS
-
-		echo "Asset '$LIBS_ZIP_FILENAME' already exists. Downloading..."
-
-		curl -s -L -o "$LIBS_ZIP_FILENAME" "$IDF_LIBS_ZIP_URL"
-		if [ $? -ne 0 ]; then
-			echo "ERROR: Failed to download asset '$LIBS_ZIP_FILENAME'"
-			exit 1
-		fi
-
-		unzip -o "$LIBS_ZIP_FILENAME" -d "$AR_OUT/tools"
-
 		JSON_ASSET_ID=`github_release_asset_id "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_JSON_FILENAME"`
-		if [ -z "$JSON_ASSET_ID" ]; then
-			echo "ERROR: JSON file '$LIBS_JSON_FILENAME' not found. Deleting asset '$LIBS_ZIP_FILENAME' and exiting..."
-			if [ "`github_release_asset_delete "$AR_LIBS_REPO" "$LIBS_ASSET_ID"`" == "0" ]; then
-				echo "ERROR: Failed to delete asset '$LIBS_ZIP_FILENAME'"
-			fi
-			exit 1
+		if [ "$JSON_ASSET_ID" != "" ] && [ `github_release_asset_delete "$AR_LIBS_REPO" "$JSON_ASSET_ID"` == "0" ]; then
+			echo "ERROR: Failed to delete asset '$LIBS_JSON_FILENAME'"
 		fi
+	fi
 
-		echo "JSON asset ID: $JSON_ASSET_ID"
-		echo "Downloading JSON asset '$LIBS_JSON_FILENAME'..."
-		curl -s -L -o "$AR_OUT/package_esp32_index.template.json" "$IDF_LIBS_JSON_URL"
+	echo "Creating asset '$LIBS_ZIP_FILENAME'..."
+
+	mv -f "dist/esp32-arduino-libs.zip" "dist/$LIBS_ZIP_FILENAME"
+	LIBS_ASSET_ID=`github_release_asset_upload "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_ZIP_FILENAME" "dist/$LIBS_ZIP_FILENAME"`
+	if [ -z "$LIBS_ASSET_ID" ]; then
+		echo "ERROR: Failed to upload asset '$LIBS_ZIP_FILENAME'"
+		exit 1
+	fi
+
+	echo "Finished uploading asset '$LIBS_ZIP_FILENAME'. Asset ID: $LIBS_ASSET_ID"
+
+	# Calculate the local file checksum and size
+	local_checksum=$(sha256sum "dist/$LIBS_ZIP_FILENAME" | awk '{print $1}')
+	local_size=$(stat -c%s "dist/$LIBS_ZIP_FILENAME")
+
+	echo "Downloading asset '$LIBS_ZIP_FILENAME' and checking integrity..."
+
+	# Download the file
+	remote_file="remote-$LIBS_ZIP_FILENAME"
+	curl -s -L -o "$remote_file" "$IDF_LIBS_ZIP_URL"
+
+	# Check if the download was successful
+	if [ $? -ne 0 ]; then
+		echo "Error downloading file from $IDF_LIBS_ZIP_URL"
+		exit 1
+	fi
+
+	# Calculate the remote file checksum and size
+	remote_checksum=$(sha256sum "$remote_file" | awk '{print $1}')
+	remote_size=$(stat -c%s "$remote_file")
+
+	echo "Local: $local_size bytes, $local_checksum"
+	echo "Remote: $remote_size bytes, $remote_checksum"
+
+	# Check if the checksums match
+	if [ "$local_checksum" != "$remote_checksum" ]; then
+		echo "Checksum mismatch for downloaded file"
+		echo "Deleting asset and exiting..."
+		if [ `github_release_asset_delete "$AR_LIBS_REPO" "$LIBS_ASSET_ID"` == "0" ]; then
+			echo "ERROR: Failed to delete asset '$LIBS_ZIP_FILENAME'"
+		fi
+		exit 1
+	fi
+
+	# Clean up the downloaded file
+	rm "$remote_file"
+
+	# Print the results
+	echo "Tool: esp32-arduino-libs"
+	echo "Version: $LIBS_VERSION"
+	echo "URL: $IDF_LIBS_ZIP_URL"
+	echo "File: $LIBS_ZIP_FILENAME"
+	echo "Size: $local_size bytes"
+	echo "SHA-256: $local_checksum"
+	echo "JSON: $AR_OUT/package_esp32_index.template.json"
+	cd "$AR_ROOT"
+	python3 tools/add_sdk_json.py -j "$AR_OUT/package_esp32_index.template.json" -n "esp32-arduino-libs" -v "$LIBS_VERSION" -u "$IDF_LIBS_ZIP_URL" -f "$LIBS_ZIP_FILENAME" -s "$local_size" -c "$local_checksum"
+	if [ $? -ne 0 ]; then exit 1; fi
+
+	JSON_ASSET_ID=`github_release_asset_upload "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "$LIBS_JSON_FILENAME" "$AR_OUT/package_esp32_index.template.json"`
+	if [ -z "$JSON_ASSET_ID" ]; then
+		echo "ERROR: Failed to upload asset '$LIBS_JSON_FILENAME'"
+		exit 1
 	fi
 fi
 
@@ -133,7 +117,7 @@ fi
 # esp32-arduino
 #
 
-if [ $AR_HAS_COMMIT == "0" ]; then
+if [ $AR_HAS_COMMIT == "0" ] || [ $LIBS_HAS_ASSET == "0" ]; then
 	cd "$AR_ROOT"
 	# create or checkout the branch
 	if [ ! $AR_HAS_BRANCH == "0" ]; then
@@ -144,7 +128,7 @@ if [ $AR_HAS_COMMIT == "0" ]; then
 		git -C "$AR_COMPS/arduino" checkout -b $AR_NEW_BRANCH_NAME
 	fi
 	if [ $? -ne 0 ]; then
-	    echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
+		echo "ERROR: Checkout of branch '$AR_NEW_BRANCH_NAME' failed"
 		exit 1
 	fi
 
@@ -157,18 +141,18 @@ if [ $AR_HAS_COMMIT == "0" ]; then
 	# did any of the files change?
 	if [ -n "$(git status --porcelain)" ]; then
 		echo "Pushing changes to branch '$AR_NEW_BRANCH_NAME'..."
-	    git add . && git commit --message "$AR_NEW_COMMIT_MESSAGE" && git push -u origin $AR_NEW_BRANCH_NAME
+		git add . && git commit --message "$AR_NEW_COMMIT_MESSAGE" && git push -u origin $AR_NEW_BRANCH_NAME
 		if [ $? -ne 0 ]; then
-		    echo "ERROR: Pushing to branch '$AR_NEW_BRANCH_NAME' failed"
+			echo "ERROR: Pushing to branch '$AR_NEW_BRANCH_NAME' failed"
 			exit 1
 		fi
 	else
-	    echo "No changes in branch '$AR_NEW_BRANCH_NAME'"
-	    if [ $AR_HAS_BRANCH == "0" ]; then
-	    	echo "Delete created branch '$AR_NEW_BRANCH_NAME'"
-	    	git branch -d $AR_NEW_BRANCH_NAME
-	    fi
-	    exit 0
+		echo "No changes in branch '$AR_NEW_BRANCH_NAME'"
+		if [ $AR_HAS_BRANCH == "0" ]; then
+			echo "Delete created branch '$AR_NEW_BRANCH_NAME'"
+			git branch -d $AR_NEW_BRANCH_NAME
+		fi
+		exit 0
 	fi
 
 	# CREATE PULL REQUEST
