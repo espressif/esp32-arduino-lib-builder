3b6b0cbd81ee9ef0886935b1fdd6fdd5696a9318
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-09-25 18:10:52 -0300

    feat(matter): script clean up

--- tools/copy-libs.sh
+++ tools/copy-libs.sh
@@ -492,15 +492,15 @@ echo -n "$LD_SCRIPTS" > "$FLAGS_DIR/ld_scripts"
 echo -n "$AR_LIBS" > "$FLAGS_DIR/ld_libs"
 
 # Matter Library adjustments
-sed 's/\\\"-DCHIP_ADDRESS_RESOLVE_IMPL_INCLUDE_HEADER=<lib\/address_resolve\/AddressResolve_DefaultImpl.h>\\\"/-DCHIP_HAVE_CONFIG_H/' $FLAGS_DIR/c_flags > $FLAGS_DIR/c_flags_temp
-mv $FLAGS_DIR/c_flags_temp $FLAGS_DIR/c_flags
-sed 's/\\\"-DCHIP_ADDRESS_RESOLVE_IMPL_INCLUDE_HEADER=<lib\/address_resolve\/AddressResolve_DefaultImpl.h>\\\"/-DCHIP_HAVE_CONFIG_H/' $FLAGS_DIR/cpp_flags > $FLAGS_DIR/cpp_flags_temp
-mv $FLAGS_DIR/cpp_flags_temp $FLAGS_DIR/cpp_flags
-sed 's/\\\"-DCHIP_ADDRESS_RESOLVE_IMPL_INCLUDE_HEADER=<lib\/address_resolve\/AddressResolve_DefaultImpl.h>\\\"/-DCHIP_HAVE_CONFIG_H/' $FLAGS_DIR/S_flags > $FLAGS_DIR/S_flags_temp
-mv $FLAGS_DIR/S_flags_temp $FLAGS_DIR/S_flags
+for flag_file in "c_flags" "cpp_flags" "S_flags"; do
+	echo "Fixing $FLAGS_DIR/$flag_file"
+ 	sed 's/\\\"-DCHIP_ADDRESS_RESOLVE_IMPL_INCLUDE_HEADER=<lib\/address_resolve\/AddressResolve_DefaultImpl.h>\\\"/-DCHIP_HAVE_CONFIG_H/' $FLAGS_DIR/$flag_file > $FLAGS_DIR/$flag_file.temp
+	mv $FLAGS_DIR/$flag_file.temp $FLAGS_DIR/$flag_file
+done
 CHIP_RESOLVE_DIR="$AR_SDK/include/espressif__esp_matter/connectedhomeip/connectedhomeip/src/lib/address_resolve"
 sed 's/CHIP_ADDRESS_RESOLVE_IMPL_INCLUDE_HEADER/<lib\/address_resolve\/AddressResolve_DefaultImpl.h>/' $CHIP_RESOLVE_DIR/AddressResolve.h > $CHIP_RESOLVE_DIR/AddressResolve_temp.h
 mv $CHIP_RESOLVE_DIR/AddressResolve_temp.h $CHIP_RESOLVE_DIR/AddressResolve.h
+# End of Matter Library adjustments
 
 # copy zigbee + zboss lib
 if [ -d "managed_components/espressif__esp-zigbee-lib/lib/$IDF_TARGET/" ]; then
