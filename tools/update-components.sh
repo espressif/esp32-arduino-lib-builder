ad918d64b174df69f0e674c29f52c59eab5afbff
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-10-01 23:04:37 -0300

    testing keyboard LED issue part 1

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -10,6 +10,11 @@ TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
+    # Temporary fix given that tinyusb/master is breaking Lib Builder
+    cd "$TINYUSB_REPO_DIR"
+    # from right before Keyboard LED problem
+    git checkout 69313ef45564cc8967575f47fb8c57371cbea470
+    cd -
 else
     git -C "$TINYUSB_REPO_DIR" fetch && \
     git -C "$TINYUSB_REPO_DIR" pull --ff-only
