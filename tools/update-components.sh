8b0baefa8aac886b9e150b789658115f9ebbe008
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-10-02 10:54:57 -0300

    revert TinyUSB version

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -10,11 +10,6 @@ TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
-    # Temporary fix given that tinyusb/master is breaking Lib Builder
-    cd "$TINYUSB_REPO_DIR"
-    # from  Sep 18, 2024
-    git checkout 40b55170c87da109b3416ac80eaa55ca56eadc77
-    cd -
 else
     git -C "$TINYUSB_REPO_DIR" fetch && \
     git -C "$TINYUSB_REPO_DIR" pull --ff-only
