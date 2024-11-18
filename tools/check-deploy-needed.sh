22ce6811335c695363a37745afec36ff4f580f79
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-11-18 09:39:44 -0300

    fix(libs): Allow for manual rebuilding and fix commit checking (#245)

--- tools/check-deploy-needed.sh
+++ tools/check-deploy-needed.sh
@@ -37,19 +37,40 @@ AR_NEW_COMMIT_MESSAGE="IDF $IDF_BRANCH $IDF_COMMIT"
 AR_NEW_PR_TITLE="IDF $IDF_BRANCH"
 
 LIBS_RELEASE_TAG="idf-"${IDF_BRANCH//\//_}""
-LIBS_VERSION="$LIBS_RELEASE_TAG-$IDF_COMMIT"
+LIBS_VERSION_PREFIX="$LIBS_RELEASE_TAG-$IDF_COMMIT-v"
+VERSION_COUNTER=1
 
 AR_HAS_BRANCH=`github_branch_exists "$AR_REPO" "$AR_NEW_BRANCH_NAME"`
 if [ "$AR_HAS_BRANCH" == "1" ]; then
-	AR_HAS_COMMIT=`github_commit_exists "$AR_REPO" "$AR_NEW_BRANCH_NAME" "$IDF_COMMIT"`
+	LATEST_LIBS_IDF=`github_get_libs_idf "$AR_REPO" "$AR_NEW_BRANCH_NAME" "$AR_NEW_PR_TITLE"`
 else
-	AR_HAS_COMMIT=`github_commit_exists "$AR_REPO" "$AR_BRANCH" "$IDF_COMMIT"`
+	LATEST_LIBS_IDF=`github_get_libs_idf "$AR_REPO" "$AR_BRANCH" "$AR_NEW_PR_TITLE"`
 fi
+
+echo "Current IDF commit: $IDF_COMMIT"
+echo "Latest IDF commit in $AR_BRANCH of $AR_REPO: $LATEST_LIBS_IDF"
+
+AR_HAS_COMMIT=`if [ "$LATEST_LIBS_IDF" == "$IDF_COMMIT" ]; then echo "1"; else echo "0"; fi`
 AR_HAS_PR=`github_pr_exists "$AR_REPO" "$AR_NEW_BRANCH_NAME"`
 
 LIBS_RELEASE_ID=`github_release_id "$AR_LIBS_REPO" "$LIBS_RELEASE_TAG"`
 LIBS_HAS_RELEASE=`if [ -n "$LIBS_RELEASE_ID" ]; then echo "1"; else echo "0"; fi`
-LIBS_ASSET_ID=`github_release_asset_id "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "esp32-arduino-libs-$LIBS_VERSION.zip"`
+
+if [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
+	echo "Workflow dispatch event. Generating new libs."
+	while true; do
+		LIBS_ASSET_ID=`github_release_asset_id "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "esp32-arduino-libs-$LIBS_VERSION_PREFIX$VERSION_COUNTER.zip"`
+		if [ -n "$LIBS_ASSET_ID" ]; then
+			VERSION_COUNTER=$((VERSION_COUNTER+1))
+		else
+			break
+		fi
+	done
+else
+	LIBS_ASSET_ID=`github_release_asset_id "$AR_LIBS_REPO" "$LIBS_RELEASE_ID" "esp32-arduino-libs-$LIBS_VERSION_PREFIX$VERSION_COUNTER.zip"`
+fi
+
+LIBS_VERSION="$LIBS_VERSION_PREFIX$VERSION_COUNTER"
 LIBS_HAS_ASSET=`if [ -n "$LIBS_ASSET_ID" ]; then echo "1"; else echo "0"; fi`
 
 export IDF_COMMIT
