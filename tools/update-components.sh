43416b0f238d0a92745dbcf04e2c25bacb5cf4c4
Garcia <rodrigo.garcia@espressif.com>
2025-03-05 17:01:54 -0300

    feat(matter): use existing folder variables in update-components.sh

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -18,7 +18,7 @@ if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Updating Matter v1.3 repository..."
 MATTER_REPO_URL="https://github.com/espressif/esp-matter.git"
-MATTER_REPO_DIR="$AR_COMPS/espressif__esp-matter"
+MATTER_REPO_DIR="$AR_COMPS/espressif__esp_matter"
 MATTER_REPO_BRANCH="release/v1.3"
 if [ ! -d "$MATTER_REPO_DIR" ]; then
     git clone --depth 1 -b "$MATTER_REPO_BRANCH" "$MATTER_REPO_URL" "$MATTER_REPO_DIR"
@@ -29,7 +29,7 @@ fi
 if [ $? -ne 0 ]; then exit 1; fi
 echo "Updating CHIP v1.3 repository..."
 CHIP_REPO_URL="https://github.com/espressif/connectedhomeip.git"
-CHIP_REPO_DIR="$AR_COMPS/espressif__esp-matter/connectedhomeip/connectedhomeip"
+CHIP_REPO_DIR="$MATTER_REPO_DIR/connectedhomeip/connectedhomeip"
 CHIP_REPO_BRANCH="v1.3-branch"
 if [ ! -d "$CHIP_REPO_DIR" ]; then
     git clone --depth 1 -b "$CHIP_REPO_BRANCH" "$CHIP_REPO_URL" "$CHIP_REPO_DIR"
@@ -40,7 +40,7 @@ else
 fi
 if [ $? -ne 0 ]; then exit 1; fi
 echo "Patching CHIP v1.3 repository..."
-CHIP_BAD_FILE="$AR_COMPS/espressif__esp-matter/connectedhomeip/connectedhomeip/src/platform/ESP32/bluedroid/ChipDeviceScanner.cpp"
+CHIP_BAD_FILE="$CHIP_REPO_DIR/src/platform/ESP32/bluedroid/ChipDeviceScanner.cpp"
 CHIP_PATCH="$AR_PATCHES/matter_chip_ChipDeviceScanner.diff"
 if [ ! -e "$CHIP_BAD_FILE" ]; then
     patch $CHIP_BAD_FILE $CHIP_PATCH
