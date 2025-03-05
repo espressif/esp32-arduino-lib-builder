b5468290515f0dc6df203ca9e6ae976e75786460
Garcia <rodrigo.garcia@espressif.com>
2025-03-05 18:07:24 -0300

    fix(matter): make the script work in different use cases

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -31,21 +31,23 @@ echo "Updating CHIP v1.3 repository..."
 CHIP_REPO_URL="https://github.com/espressif/connectedhomeip.git"
 CHIP_REPO_DIR="$MATTER_REPO_DIR/connectedhomeip/connectedhomeip"
 CHIP_REPO_BRANCH="v1.3-branch"
-if [ ! -d "$CHIP_REPO_DIR" ]; then
-    git clone --depth 1 -b "$CHIP_REPO_BRANCH" "$CHIP_REPO_URL" "$CHIP_REPO_DIR"
-    $CHIP_REPO_DIR/scripts/checkout_submodules.py --platform esp32 darwin --shallow
-else
-    git -C "$MATTER_REPO_DIR" fetch && \
-    git -C "$MATTER_REPO_DIR" pull --ff-only
+if [ -d "$CHIP_REPO_DIR" ]; then
+    # Check if directory is empty
+    if [ -z "$(ls -A "$CHIP_REPO_DIR")" ]; then
+        rm -rf "$CHIP_REPO_DIR"
+        git clone --depth 1 -b "$CHIP_REPO_BRANCH" "$CHIP_REPO_URL" "$CHIP_REPO_DIR"
+        if [ $? -ne 0 ]; then exit 1; fi
+        $CHIP_REPO_DIR/scripts/checkout_submodules.py --platform esp32 darwin --shallow
+        if [ $? -ne 0 ]; then exit 1; fi
+        echo "Patching CHIP v1.3 repository..."
+        CHIP_BAD_FILE="$CHIP_REPO_DIR/src/platform/ESP32/bluedroid/ChipDeviceScanner.cpp"
+        CHIP_PATCH="$AR_PATCHES/matter_chip_ChipDeviceScanner.diff"
+        if [ -f "$CHIP_BAD_FILE" ]; then
+            patch $CHIP_BAD_FILE $CHIP_PATCH
+        else
+            echo "Error: $CHIP_BAD_FILE not found. Check the script."
+            exit 1
+        fi
+    fi
 fi
-if [ $? -ne 0 ]; then exit 1; fi
-echo "Patching CHIP v1.3 repository..."
-CHIP_BAD_FILE="$CHIP_REPO_DIR/src/platform/ESP32/bluedroid/ChipDeviceScanner.cpp"
-CHIP_PATCH="$AR_PATCHES/matter_chip_ChipDeviceScanner.diff"
-if [ ! -e "$CHIP_BAD_FILE" ]; then
-    patch $CHIP_BAD_FILE $CHIP_PATCH
-else
-    echo "Error: $CHIP_BAD_FILE not found. Check the script."
-fi
-if [ $? -ne 0 ]; then exit 1; fi
 echo "Matter v1.3 component is installed and updated."
