4f1e84471179bb6653c269ddc4ecb11ee1cc516b
me-no-dev <hristo@espressif.com>
2025-11-06 23:15:05 +0200

    Clean TinyUSB component

--- tools/patch-tinyusb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mv components/arduino_tinyusb/src/dcd_dwc2.c components/arduino_tinyusb/src/dcd_dwc2.c.prev
-cp components/arduino_tinyusb/tinyusb/src/portable/synopsys/dwc2/dcd_dwc2.c components/arduino_tinyusb/src/dcd_dwc2.c
-patch -p1 -N -i components/arduino_tinyusb/patches/dcd_dwc2.patch
